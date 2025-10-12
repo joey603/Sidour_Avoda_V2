7bb9e1e48f756061d463b40c40ee648c236cbf6c
Yoeli Barthel <yoelibarthel603@gmail.com>
2025-10-12 08:16:40 +0300

    chore(version): bump to 1.0.83

--- Sidour-avoda-Tzora-chevron/interface_2.py
+++ Sidour-avoda-Tzora-chevron/interface_2.py
@@ -15,7 +15,7 @@ import datetime
 
 class InterfacePlanning:
     # Version du projet
-    VERSION = "1.0.82"
+    VERSION = "1.0.83"
     
     def __init__(self, repos_minimum_entre_gardes=8):
         self.repos_minimum_entre_gardes = repos_minimum_entre_gardes
